eb81acb54029d6b0ed9d1227eebe3bbbefda24f9
lexandreOliveira <A-35-O-RED@protonmail.com>
2023-05-15 19:00:14 -0300

    first commit

---
+++ aula01/app1.js
@@ -0,0 +1,10 @@
+// Comentário de uma linha
+/* Comentário de bloco de
+                          
+                        linha!
+
+JavaScript é uma linguagem interpletada (Executada em tempo real, 
+não precisa compilar o código)*/
+
+
+console.log("Hello World")
---
+++ aula02/app2.js
@@ -0,0 +1,10 @@
+/* Aula 02 de JavaScript Moderno, do CFBcursos!
+JavaScript em modo estrito
+Hoje em nosso curso de Javascript Completo e Profissional 
+vamos aprender o que é o modo estrito, usando o comando "use strict". 
+O modo estrito ou strict mode faz com que a programação seja mais limpa e funcional.
+
+JavaScript é uma linguagem de programação interpretada estruturada, 
+de script em alto nível com tipagem dinâmica fraca e multiparadigma. 
+Juntamente com HTML e CSS, o JavaScript é uma das três principais 
+tecnologias para desenvolvimento de aplicações Web e Sites. */
